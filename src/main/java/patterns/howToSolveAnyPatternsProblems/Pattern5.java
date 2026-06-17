1bba3fb308c5163660d10cac93e0028e02d62191
bhumi-rastogi <bhumi.rastogi05@gmail.com>
2026-06-18 00:00:50 +0530

    Day 8: Pattern Problems

---
+++ src/main/java/patterns/howToSolveAnyPatternsProblems/Pattern5.java
@@ -0,0 +1,14 @@
+package patterns.howToSolveAnyPatternsProblems;
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int total_no_of_lines = n;
+        int current_no_of_lines = 1;
+        int stars = 1;
+        int spaces = 
+    }
+}
